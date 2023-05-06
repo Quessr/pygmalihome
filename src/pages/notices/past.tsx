13167f504accecae5a0076856c4752774f570cd8
Quessr <Quessr00@gmail.com>
2023-05-06 18:07:36 +0900

    fix: 신청일 지난공고 보여주기 기능

--- src/pages/notices/past.tsx
+++ src/pages/notices/past.tsx
@@ -1,3 +1,4 @@
+import CardList from "@/common/CardList";
 import FeedCardList, { FeedCardProps } from "@/components/Feed/FeedCardList";
 import axios from "axios";
 import dayjs from "dayjs";
@@ -7,21 +8,21 @@ export async function getStaticProps() {
   const res = await axios.get(
     "https://pygmalihome-backend.vercel.app/api/housing/subscription",
     {
-      params: { limit: 20 },
+      params: { limit: 20, toStartDate: "2023-05-04", isReceving: false },
     }
   );
-  const pastNotice: Array<FeedCardProps> = res.data.data.filter(
-    (item: FeedCardProps) => {
-      return (
-        item.endDate !== undefined &&
-        item.endDate < dayjs().format("YYYY-MM-DD")
-      );
-    }
-  );
+  // const pastNotice: Array<FeedCardProps> = res.data.data.filter(
+  //   (item: FeedCardProps) => {
+  //     return (
+  //       item.endDate !== undefined &&
+  //       item.endDate < dayjs().format("YYYY-MM-DD")
+  //     );
+  //   }
+  // );
 
   return {
     props: {
-      pastNotice,
+      pastNotice: res.data.data,
     },
     revalidate: 10, // In seconds
   };
@@ -30,12 +31,10 @@ export async function getStaticProps() {
 const PastNoticePage = ({
   pastNotice,
 }: InferGetStaticPropsType<typeof getStaticProps>) => {
+  console.log("pastNotice", pastNotice);
   return (
     <div>
-      <FeedCardList
-        title="신청 기한이 지난 공고 입니다!"
-        cardList={pastNotice}
-      />
+      <CardList title="신청 기한이 지난 공고 입니다." cardList={pastNotice} />
     </div>
   );
 };
