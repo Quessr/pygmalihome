c437db90771e29cff7dfa413b9263667da4016fe
Quessr <Quessr00@gmail.com>
2023-05-12 22:24:27 +0900

    fix: 이번달 신청가능 공고 보여주기 기능 수정

--- src/components/Aside/index.tsx
+++ src/components/Aside/index.tsx
@@ -6,64 +6,94 @@ import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import dayjs from "dayjs";
 
+const getParamList = () => {
+  const startOfMonth = dayjs().startOf("month");
+  const endOfMonth = dayjs().endOf("month");
+  return [
+    {
+      type: "sh",
+      fromStartDate: dayjs().format("YYYY-MM-DD"),
+      toStartDate: endOfMonth.format("YYYY-MM-DD"),
+    },
+    {
+      type: "lh",
+      fromStartDate: dayjs().format("YYYY-MM-DD"),
+      toStartDate: endOfMonth.format("YYYY-MM-DD"),
+    },
+    {
+      type: "sh",
+      fromStartDate: startOfMonth.format("YYYY-MM-DD"),
+      toStartDate: dayjs().subtract(1, "day").format("YYYY-MM-DD"),
+      isReceiving: true,
+    },
+    {
+      type: "lh",
+      fromStartDate: startOfMonth.format("YYYY-MM-DD"),
+      toStartDate: dayjs().subtract(1, "day").format("YYYY-MM-DD"),
+      isReceiving: true,
+    },
+  ];
+};
+
 const Aside: FC = () => {
   const { data: youtubeList, isLoading } = useQuery({
     queryKey: ["youtube"],
     queryFn: () => axios.get("/api/housing/youtube").then((res) => res.data),
   });
 
-  const { data: thisMonthNoticesCount } = useQuery<Array<EachNoticeCountProps>>(
-    {
-      queryKey: ["subscription"],
-      queryFn: async () => {
-        const endOfMonth = dayjs().endOf("month");
-        const requests = ["sh", "lh"].map((type) => {
-          return axios
-            .get("/api/housing/subscription", {
-              params: {
-                type,
-                fromStartDate: dayjs().format("YYYY-MM-DD"),
-                toStartDate: endOfMonth.format("YYYY-MM-DD"),
-              },
-            })
-            .then((res) => res.data)
-            .catch((error) => {
-              return Promise.resolve({
-                total: 0,
-                error: error.message,
-              });
+  const { data: thisMonthNoticesCount } = useQuery({
+    queryKey: ["subscriptionCount"],
+    queryFn: async () => {
+      const paramList = getParamList();
+      const requestList = paramList.map((params) => {
+        return axios
+          .get("/api/housing/subscription", {
+            params,
+          })
+          .then((res) => {
+            return {
+              type: params.type,
+              total: Number(res.data.total),
+              error: null,
+            };
+          })
+          .catch((error) => {
+            return Promise.resolve({
+              type: params.type,
+              total: 0,
+              error: error.message,
             });
-        });
-        const response = await Promise.allSettled(requests);
-        const result = response.map((response, i) => {
-          if (response.status === "fulfilled") {
-            const type = ["sh", "lh"][i];
-            const { total } = response.value;
-            return {
-              type,
-              count: total,
-            };
-          } else {
-            const type = ["sh", "lh"][i];
-            return {
-              type,
-              count: 0,
-            };
-          }
-        });
-        return result;
-      },
-    }
-  );
+          });
+      });
+      const responseList = await Promise.allSettled(requestList);
+      const result = responseList.map((response) => {
+        if (response.status === "fulfilled") {
+          const {
+            value: { type, total },
+          } = response;
+          return {
+            type,
+            count: total,
+          };
+        } else {
+          return null;
+        }
+      });
+      return Object.entries(
+        result.reduce((acc, cur) => {
+          if (cur) acc[cur.type] = (acc[cur.type] ?? 0) + cur.count;
+          return acc;
+        }, {} as Record<string, number>)
+      ).map(([type, count]) => ({ type, count }));
+    },
+  });
 
   return (
     <div
       css={css`
         display: grid;
-        /* grid-template-rows: repeat(2, 1fr 3fr); */
         grid-template-rows: 124px 350px;
         gap: 24px;
-        /* margin-top: 28px; */
         margin-left: 28px;
       `}
     >
